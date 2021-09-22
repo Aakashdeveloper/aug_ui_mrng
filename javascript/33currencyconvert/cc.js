ce815d52d51021f88c5cd7354ebe8bc9725110f5
aakashdeveloper <ahanda205@gmail.com>
2021-09-22 07:46:03 +0530

    geolocation

---
+++ javascript/33currencyconvert/cc.js
@@ -0,0 +1,31 @@
+const url = "http://data.fixer.io/api/latest?access_key=10b816fb390183d161cc164e53490b9c";
+/*
+//es5
+function convert(){
+    var out = document.getElementById('out').value;
+    var amount = document.getElementById('amount').value;
+    // call api 
+    fetch(url)
+    // return the promise
+    .then((res) =>res.json())
+    // return data
+    .then((data) => {
+        var price = data.rates[out];
+        var final = price * Number(amount)
+        var result = `Converted Price of ${amount} Euro is ${final} ${out}`
+        document.getElementById('output').innerText = result;
+    })
+}*/
+
+//es6
+
+async function convert(){
+    var out = document.getElementById('out').value;
+    var amount = document.getElementById('amount').value;
+    let response = await fetch(url)
+    let data  = await response.json()
+    var price = data.rates[out];
+    var final = price * Number(amount)
+    var result = `Converted Price of ${amount} Euro is ${final} ${out}`
+    document.getElementById('output').innerText = result;
+}
