96d972bb5e4d45a029a4f6dc7aa1ef448d633742
aakashdeveloper <ahanda205@gmail.com>
2021-09-25 07:41:46 +0530

    callapply

---
+++ javascript/40Jquery/first.js
@@ -0,0 +1,12 @@
+$('h1')
+m.fn.init [h1, prevObject: m.fn.init(1), context: document, selector: 'h1']
+$('h1')[0]
+<h1>​I'm AAKASH HANDA​</h1>​
+$('h1')[0].innerText
+"I'm AAKASH HANDA"
+$('h1')[0].innerText="Hello"
+'Hello'
+$('.scroll')[0]
+<a href=​"#skills" class=​"scroll">​Skills​</a>​
+$('.scroll')[0].innerText="Tech"
+'Tech'
